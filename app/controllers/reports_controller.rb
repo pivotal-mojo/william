39dbdf57091bc6ffabd265c7dcbc1f10ff5980c9
Gregg Van Hove and Steven McAlpine <pair+gvanhove+smcalpine@pivotal.io>
2016-02-12 17:05:50 -0800

    Add export as CSV to the report
    [finish #113112947]

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,28 @@
+require 'csv'
+
 class ReportsController < ApplicationController
+  respond_to :html, :csv
+
   def show
     @all_groups = Group.all
+
+    respond_to do |format|
+      format.html { render }
+      format.csv { render csv: generate_csv(@all_groups) }
+    end
+  end
+
+  private
+
+  def generate_csv(groups)
+    CSV.generate(headers: ['Group Name', 'Project Name', 'VM Name', 'CPUs', 'Memory (MB)', 'Storage (GB)'], write_headers: true) do |csv|
+      groups.each do |g|
+        g.projects.each do |p|
+          p.vms.each do |vm|
+            csv << [g.name, p.name, vm.name, vm.cpus, vm.memory, vm.storage, 1]
+          end
+        end
+      end
+    end
   end
 end
---
+++ config/initializers/csv_responder.rb
@@ -0,0 +1,5 @@
+require 'action_controller/metal/renderers'
+
+ActionController.add_renderer :csv do |csv, options|
+  self.response_body = csv.respond_to?(:to_csv) ? csv.to_csv(options) : csv
+end
